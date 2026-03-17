f872aeadfa901b61452f00a8cdafd4b2aa28725e
Iddo Lev <iddolev9@gmail.com>
2026-03-17 10:17:26 +0200

    apf_install

--- installation/apf_install.bat
+++ installation/apf_install.bat
@@ -1,2 +1,3 @@
 @echo off
-REM to do: fetch the file https://github.com/iddolev/apf/blob/main/apf_install.
+curl -sL -o apf_install.py https://raw.githubusercontent.com/iddolev/apf/main/installation/apf_install.py
+python apf_install.py
