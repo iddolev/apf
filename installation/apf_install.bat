0ca0c85ef84ca1535e92d0ae7f4a4879ce2dbd6d
Iddo Lev <iddolev9@gmail.com>
2026-03-17 23:46:32 +0200

    .

--- installation/apf_install.bat
+++ installation/apf_install.bat
@@ -2,4 +2,7 @@
 REM This is the file to share with anyone who wants to install APF in their project folder
 
 curl -sL -o apf_install.py https://raw.githubusercontent.com/iddolev/apf/main/installation/apf_install.py
+
+for %%a in (%*) do if "%%a"=="--import" exit /b 0
+
 python apf_install.py %*
