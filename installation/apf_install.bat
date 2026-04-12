e9f0dad99ff8377bf2587becd702dade590b18a7
Iddo Lev <iddolev9@gmail.com>
2026-04-13 00:31:09 +0300

    apf_install.bat

--- installation/apf_install.bat
+++ installation/apf_install.bat
@@ -1,14 +1,14 @@
 @echo off
 REM This is the file to share with anyone who wants to install APF in their project folder
 
-if not exist ".apf" mkdir ".apf"
+if not exist ".apf\installation" mkdir ".apf\installation"
 
-curl -sL -o ".apf\apf_install.py" https://raw.githubusercontent.com/iddolev/apf/main/installation/apf_install.py
+curl -sL -o ".apf\installation\apf_install.py" https://raw.githubusercontent.com/iddolev/apf/main/installation/apf_install.py
 
 for %%a in (%*) do if "%%a"=="--fetch" (
     echo Fetched apf_install.py
     exit /b 0
 )
 
-pip install pyyaml
-python ".apf\apf_install.py" %*
+pip install pyyaml ruamel.yaml
+python ".apf\installation\apf_install.py" %*
